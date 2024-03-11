1c55a06474c4d8b4fbe301fbadf85668a125199e
bwng0v0 <ehs4742@gmail.com>
2024-03-11 20:57:15 +0900

    3.11

---
+++ spare/s1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+//https://www.acmicpc.net/problem/2741
+int main(){
+using namespace std;
+
+int n;
+cin>>n;
+for(int i=1; i<=n; i++)
+cout<<i<<'\n';
+
+    return 0;
+}
---
+++ spare/s2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+//https://www.acmicpc.net/problem/2742
+int main(){
+using namespace std;
+
+int n;
+cin>>n;
+for(int i=n; i>0; i--)
+cout<<i<<'\n';
+
+    return 0;
+}
