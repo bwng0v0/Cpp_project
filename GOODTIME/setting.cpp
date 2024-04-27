1ac4a8c4d0c885f2ca730456c75dc84b410ec6c9
bwng0v0 <ehs4742@gmail.com>
2024-04-28 00:41:46 +0900

    setting

--- GOODTIME/setting.cpp
+++ GOODTIME/setting.cpp
@@ -1,9 +1,11 @@
 //
 #include <iostream>
+#define fastio ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
 using namespace std;
 
 int main(){
-ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+fastio
+
 
 
     return 0;
