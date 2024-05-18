eee16f71081895b3d7d225d29ce2cd736f5ab91b
bwng0v0 <ehs4742@gmail.com>
2024-05-19 01:49:56 +0900

    5.17 spare solve

---
+++ 2024.5/5.17spare.cpp
@@ -0,0 +1,12 @@
+//https://www.acmicpc.net/problem/27331 2 桁の整数 (Two-digit Integer)
+#include <iostream>
+#define fastio ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+using namespace std;
+
+int main(){
+fastio
+
+int a,b; cin>>a>>b; cout<<a<<b;
+
+    return 0;
+}
