907f834cb38fd6cb5da5908ed7872db5ce021d10
bwng0v0 <ehs4742@gmail.com>
2024-05-24 01:18:46 +0900

    5.23 use spare

---
+++ 2024.5/5.23spare.cpp
@@ -0,0 +1,12 @@
+//https://www.acmicpc.net/problem/24082 立方体 (Cube)
+#include <iostream>
+#define fastio ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+using namespace std;
+
+int main(){
+fastio
+
+int n; cin>>n; cout<<n*n*n;
+
+    return 0;
+}
