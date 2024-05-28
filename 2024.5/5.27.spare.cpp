27c003f094ac5f29c5a41fd51888b2dac12698eb
bwng0v0 <ehs4742@gmail.com>
2024-05-28 23:48:09 +0900

    5.27 spare

---
+++ 2024.5/5.27.spare.cpp
@@ -0,0 +1,13 @@
+//https://www.acmicpc.net/problem/24078 余り (Remainder)
+#include <iostream>
+#define fastio ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+using namespace std;
+
+int main(){
+fastio
+
+int x; cin>>x;
+cout<<x%21;
+
+    return 0;
+}
