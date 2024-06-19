5d51ac4e2beb816ab4064bfe5d85474f150483da
bwng0v0 <ehs4742@gmail.com>
2024-06-20 00:50:24 +0900

    6.19 solve

---
+++ 2024.6/6.19.cpp
@@ -0,0 +1,13 @@
+//https://www.acmicpc.net/problem/15963
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+using namespace std;
+
+int main(){
+fastio
+
+long long int a,b; cin>>a>>b;
+cout<<(a==b);
+
+    return 0;
+}
