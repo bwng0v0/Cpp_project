b8d5dc8493a38f22af2caa136a5ac86f5ab83c7d
bwng0v0 <ehs4742@gmail.com>
2024-07-30 12:13:54 +0900

    setting add X,Y

--- setting.cpp
+++ setting.cpp
@@ -1,6 +1,8 @@
 //
 #include <bits/stdc++.h>
 #define fastio ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#define X first
+#define Y second
 using namespace std;
 
 int main(){
