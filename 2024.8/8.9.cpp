80c5c2aa100ba976ff235333886d759b13d9f5e0
bwng0v0 <ehs4742@gmail.com>
2024-09-19 09:43:45 +0900

    8.9 solve

---
+++ 2024.8/8.9.cpp
@@ -0,0 +1,22 @@
+//https://www.acmicpc.net/problem/2777 숫자 놀이
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#define X first
+#define Y second
+using namespace std;
+
+int main(){
+fastio
+
+int T,n; cin>>T;
+for( int t=0; t<T; t++ ){
+    cin>>n;
+    //9 이하의 약수의 종류
+    int div=2;
+    while(div<=9){
+        
+    }
+}
+
+    return 0;
+}
